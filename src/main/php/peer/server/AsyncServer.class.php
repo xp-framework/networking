a48a8a948121a779a69c5ee1b953976278076b6b
Timm Friebe <thekid@thekid.de>
2021-03-29 19:47:10 +0200

    Implement asynchronous server
    It supports protocols where handleData() returns a generator function to yield
    back control to the server, e.g. during lengthy up- or download operations.
    
    For backwards compatibility, this is implemented in a new server class called
    peer.server.AsyncServer. However, this will become the default with the next
    major release

---
+++ src/main/php/peer/server/AsyncServer.class.php
@@ -0,0 +1,202 @@
+<?php namespace peer\server;
+
+use lang\{IllegalStateException, Throwable};
+use peer\server\protocol\SocketAcceptHandler;
+use peer\{ServerSocket, SocketTimeoutException};
+
+/**
+ * Asynchronous TCP/IP Server
+ *
+ * ```php
+ * use peer\server\AsyncServer;
+ *   
+ * $server= new AsyncServer();
+ * $server->listen(new ServerSocket('127.0.0.1', 6100), new MyProtocol());
+ * $server->service();
+ * $server->shutdown();
+ * ```
+ *
+ * @see   xp://peer.ServerSocket
+ * @test  xp://peer.unittest.server.ServerTest
+ */
+class AsyncServer extends Server {
+  private $select= [], $handle= [], $tasks= [];
+
+  /**
+   * Adds server socket to listen on, associating protocol handler with it
+   *
+   * @param  peer.ServerSocket|peer.BSDServerSocket $socket
+   * @param  peer.server.ServerProtocol $protocol
+   * @return self
+   */
+  public function listen($socket, $protocol) {
+    $protocol->server= $this;
+
+    $socket->create();
+    $socket->bind(true);
+    $socket->listen();
+
+    $this->select[]= $socket;
+    $this->handle[]= [function($socket) use($protocol) {
+      $connection= $socket->accept();
+      if ($protocol instanceof SocketAcceptHandler && !$protocol->handleAccept($connection)) {
+        $connection->close();
+        return;
+      }
+
+      $this->tcpnodelay && $connection->useNoDelay();
+      $protocol->handleConnect($connection);
+
+      $this->select[]= $connection;
+      $this->handle[]= [
+        [$protocol, 'handleData'],
+        [$protocol, 'handleDisconnect'],
+        [$protocol, 'handleError'],
+        [$protocol, 'initialize']
+      ];
+    }];
+
+    return $this;
+  }
+
+  /**
+   * Shutdown the server
+   *
+   * @return void
+   */
+  public function shutdown() {
+    $this->terminate= true;
+    while ($socket= array_pop($this->select)) {
+      $socket->close();
+    }
+  }
+
+  /**
+   * Adds socket to select, associating a function to call for data
+   *
+   * @param  peer.Socket|peer.BSDSocket $socket
+   * @param  function(peer.Socket|peer.BSDSocket): void $function
+   * @return peer.Socket|peer.BSDSocket
+   */
+  public function select($socket, $function) {
+    $this->select[]= $socket;
+    $this->handle[]= [$function];
+    return $socket;
+  }
+
+  /**
+   * Schedule a given task to execute every given seconds
+   *
+   * @param  int $seconds
+   * @param  function(): void
+   */
+  public function schedule($seconds, $function) {
+    $this->tasks[-1 - sizeof($this->tasks)]= [$seconds, $function];
+  } 
+
+  /**
+   * Runs service until shutdown() is called.
+   *
+   * @return void
+   * @throws lang.IllegalStateException
+   */
+  public function service() {
+    if (empty($this->select)) {
+      throw new IllegalStateException('No sockets to select on');
+    }
+
+    // Initialize handles if necessary
+    foreach ($this->handle as $handle) {
+      if ($f= $handle[3] ?? null) $f();
+    }
+
+    // Set up scheduled tasks
+    $time= time();
+    $next= $cont= [];
+    foreach ($this->tasks as $i => $task) {
+      $next[$i]= $time + $task[0];
+    }
+
+    $null= null;
+    $sockets= $this->select[0]->kind();
+    do {
+
+      // Build array of sockets that we want to check for data. If one of them
+      // has disconnected in the meantime, notify the listeners (socket will be
+      // already invalid at that time) and remove it from the clients list.
+      $read= [];
+      foreach ($this->select as $i => $socket) {
+        if (!$socket->isConnected()) {
+          if ($f= $this->handle[$i][1] ?? null) $f($socket);
+          unset($this->select[$i], $this->handle[$i], $next[$i]);
+          continue;
+        }
+
+        // Do not re-enter handler as long as we have a continuation
+        if (isset($cont[$i])) continue;
+
+        // Handle timeouts manually instead of leaving this up to the sockets
+        // themselves - the latter has proven not to be 100% reliable.
+        if (isset($next[$i]) && $next[$i] <= $time) {
+          if ($f= $this->handle[$i][2] ?? null) {
+            $f($socket, new SocketTimeoutException('Timed out', $socket->getTimeout()));
+            $socket->close();
+            unset($this->select[$i], $this->handle[$i], $next[$i]);
+            continue;
+          }
+          $next[$i]= $time + $socket->getTimeout();
+        }
+
+        $read[$i]= $socket;
+      }
+      // echo '* SELECT (', \util\Objects::stringOf($next), ")\n";
+      $sockets->select($read, $null, $null, $next ? max(0, min($next) - $time) : null);
+
+      // Run scheduled tasks, recording their next run immediately thereafter
+      $time= time();
+      foreach ($this->tasks as $i => $task) {
+        if ($next[$i] <= $time) {
+          $next[$i]= $time + $task[0];
+          $task[1]();
+        }
+      }
+
+      // There is data on the server socket (meaning a client connection is
+      // waiting to be socket), or on any of the other sockets, so we'll call
+      // into their respective data handler
+      foreach ($read as $i => $socket) {
+        try {
+          $r= $this->handle[$i][0]($socket);
+          if ($r instanceof \Generator) {
+            $cont[$i]= true;
+            $task= -1 - sizeof($this->tasks);
+            $next[$task]= $time;
+
+            $this->tasks[$task]= [0, function() use($r, $i, $task, &$cont, &$next) {
+              if ($r->valid()) {
+                $r->next();
+              } else {
+                unset($this->tasks[$task], $next[$task], $cont[$i]);
+              }
+            }];
+          }
+
+          $next[$i]= $time + $socket->getTimeout();
+        } catch (Throwable $t) {
+          if ($f= $this->handle[$i][2] ?? null) $f($socket, $t);
+        }
+      }
+
+      $time= time();
+    } while (!$this->terminate);
+  }
+
+  /**
+   * Creates a string representation
+   *
+   * @return  string
+   */
+  public function toString() {
+    return nameof($this);
+  }
+}
--- src/main/php/peer/server/Server.class.php
+++ src/main/php/peer/server/Server.class.php
@@ -10,7 +10,6 @@ use peer\{BSDServerSocket, ServerSocket};
  *   
  * $server= new Server();
  * $server->listen(new ServerSocket('127.0.0.1', 6100), new MyProtocol());
- * $server->init();
  * $server->service();
  * $server->shutdown();
  * ```
@@ -55,6 +54,10 @@ class Server {
   public function listen($socket, ServerProtocol $protocol) {
     $protocol->server= $this;
 
+    $socket->create();
+    $socket->bind(true);
+    $socket->listen();
+
     $this->socket= $socket;
     $this->protocol= $protocol;
     return $this;
@@ -63,21 +66,21 @@ class Server {
   /**
    * Initialize the server
    *
+   * @deprecated
    */
   public function init() {
-    $this->socket->create();
-    $this->socket->bind(true);
-    $this->socket->listen();
+    // NOOP
   }
   
   /**
    * Shutdown the server
    *
+   * @return void
    */
   public function shutdown() {
-    $this->server->terminate= true;
+    $this->terminate= true;
     $this->socket->close();
-    $this->server->terminate= false;
+    $this->terminate= false;
   }
   
   /**
@@ -187,7 +190,7 @@ class Server {
         $index= (int)$handle;
         $lastAction[$index]= $currentTime;
         try {
-          $this->protocol->handleData($handles[$index]);
+          foreach ($this->protocol->handleData($handles[$index]) ?? [] as $_) { }
         } catch (\io\IOException $e) {
           $this->protocol->handleError($handles[$index], $e);
           $handles[$index]->close();
